da4c3f60b08183981d4d5ab36b7fd88e8019e473
Aris Chow <hello@arisc.how>
2023-08-14 10:17:32 +0800

    better layout & key mappings for daily work

--- home/arischow/nixvim.nix
+++ home/arischow/nixvim.nix
@@ -4,16 +4,44 @@
     options = {
       number = true;
     };
+    maps = {
+      normalVisualOp.";" = ":"; # Same as noremap ; :     
+      normal."<C-[>" = {
+        silent = true;
+        action = ":bprevious<CR>";
+      };
+      normal."<C-]>" = {
+        silent = true;
+        action = ":bnext<CR>";
+      };
+    };
     plugins = {
+      bufferline = {
+        enable = true;
+      };
       nvim-tree = {
         enable = true;
 
         autoClose = true;
         disableNetrw = true;
-        openOnSetup = true;
       };
       lualine = {
         enable = true;
+	sections.lualine_c = [{
+	  name = "filename";
+	  extraConfig = {
+	    path = 1;
+	  };
+	}];
+      };
+      telescope = {
+        enable = true;
+        keymaps = {
+            "<C-o>" = {
+              action = "git_files";
+              desc = "Telescope Git Files";
+            };
+          };
       };
       toggleterm = {
         enable = true;
