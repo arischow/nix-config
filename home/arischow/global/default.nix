de726b5d8c28fdd52e30ea2be83ce705d5fb6919
Aris Chow <hello@arisc.how>
2023-08-14 10:18:24 +0800

    add a patched nerd font that bufferline.nvim depends

--- home/arischow/global/default.nix
+++ home/arischow/global/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ../features/cli
+    ../features/fonts
   ];
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
