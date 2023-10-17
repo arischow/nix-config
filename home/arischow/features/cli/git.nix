5fa7f80b1527770f581669a0e5933579d29225e2
Aris Chow <hello@arisc.how>
2023-10-17 13:39:23 +0800

    feat(git): Organizing multiple Git identities
    Reference: https://garrit.xyz/posts/2023-10-13-organizing-multiple-git-identities

--- home/arischow/features/cli/git.nix
+++ home/arischow/features/cli/git.nix
@@ -1,8 +1,19 @@
 {
   programs.git = {
     enable = true;
-    userEmail = null;
-    userName = null;
+    userEmail = "hello@arisc.how";
+    userName = "Aris Chow";
+    includes = [
+      {
+        condition = "gitdir:~/Work/repos/Mintel/";
+        contents = {
+          user = {
+            email = "azhou@mintel.com";
+            name = "Aris Zhou";
+          };
+        };
+      }
+    ];
   };
   programs.git.diff-so-fancy = {
     enable = true;
