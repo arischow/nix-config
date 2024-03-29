0cbbcccd7d1ada56a5cf965edc6af777ade9f08e
Aris Chow <hello@arisc.how>
2024-03-29 10:46:15 +0800

    add(fish): homebrew

--- home/arischow/features/cli/fish.nix
+++ home/arischow/features/cli/fish.nix
@@ -2,6 +2,12 @@
   programs = {
     fish = {
       enable = true;
+      loginShellInit = ''
+      # brew
+      if test -d /opt/homebrew/bin/brew
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+      end
+      '';
     };
   };
 }
