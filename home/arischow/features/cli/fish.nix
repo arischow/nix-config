050599127cc179406cdc930ca10f25fdf990776b
Aris Chow <hello@arisc.how>
2024-03-31 07:54:25 +0800

    add(fish): poetry completion support
    https://github.com/python-poetry/poetry/issues/5929

--- home/arischow/features/cli/fish.nix
+++ home/arischow/features/cli/fish.nix
@@ -10,6 +10,10 @@
 
         # pyenv
         pyenv init - | source
+
+        # poetry
+        poetry completions fish > ~/.config/fish/completions/poetry.fish
+        sed -i.bak -E "s/'([a-z]*[[:blank:]][a-z]*)''''/\1'/g" /opt/homebrew/share/fish/vendor_completions.d/poetry.fish
       '';
     };
   };
