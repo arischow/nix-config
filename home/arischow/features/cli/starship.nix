0ce453265c6d792f478d71e3cdfafe447d6f8b79
Aris Chow <hello@arisc.how>
2024-03-31 07:55:33 +0800

    add(starship): Everything should be catppuccin :D
    I am not sure if it works - I think iTerm2 would load its own configuration instead.

--- home/arischow/features/cli/starship.nix
+++ home/arischow/features/cli/starship.nix
@@ -1,6 +1,22 @@
+{ pkgs, ... }:
 {
-  programs.starship = {
-    enable = true;
-    settings = (builtins.fromTOML (builtins.readFile ./starship.toml));
-  };
+  programs.starship =
+    let
+      flavour = "macchiato";
+    in
+    {
+      enable = true;
+      settings = {
+        format = "$all";
+        palette = "catppuccin_${flavour}";
+      } // (builtins.fromTOML (builtins.readFile ./starship.toml))
+      // builtins.fromTOML (builtins.readFile
+        (pkgs.fetchFromGitHub
+          {
+            owner = "catppuccin";
+            repo = "starship";
+            rev = "5629d23"; # Replace with the latest commit hash
+            sha256 = "sha256-nsRuxQFKbQkyEI4TXgvAjcroVdG+heKX5Pauq/4Ota0=";
+          } + /palettes/${flavour}.toml));
+    };
 }
