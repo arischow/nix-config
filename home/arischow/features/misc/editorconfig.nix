2a11835cff1fc935d627d56c3059c076b1d859e2
Aris Chow <hello@arisc.how>
2024-03-31 08:01:37 +0800

    add(editorconfig): merge configuration from gomashio

--- home/arischow/features/misc/editorconfig.nix
+++ home/arischow/features/misc/editorconfig.nix
@@ -3,6 +3,10 @@
     enable = true;
     settings = {
       "*" = {
+        charset = "utf-8";
+        end_of_line = "lf";
+        insert_final_newline = true;
+        trim_trailing_whitespace = true;
         indent_style = "space";
         indent_size = 4;
       };
@@ -15,6 +19,10 @@
       };
       # JavaScript
       "*.{js,jsx,ts,tsx}" = {
+        indent_size = 2;
+      };
+      # HTML and CSS
+      "*.{html,css,scss}" = {
         indent_size = 2;
       };
       "Makefile" = {
