33cd771bed6fd45599c9e32e518d3d9504abb779
Aris Chow <hello@arisc.how>
2024-04-19 13:26:10 +0800

    feat(fish): nvm support

--- home/arischow/features/cli/fish.nix
+++ home/arischow/features/cli/fish.nix
@@ -14,6 +14,17 @@
         pyenv init - | source
 
       '';
+      plugins = [
+        {
+          name = "nvm";
+          src = pkgs.fetchFromGitHub {
+            owner = "jorgebucaran";
+            repo = "nvm.fish";
+            rev = "master";
+            sha256 = "LV5NiHfg4JOrcjW7hAasUSukT43UBNXGPi1oZWPbnCA=";
+          };
+        }
+      ];
     };
   };
 }
